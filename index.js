c5de1ad27e3834e151ba7e2620b89c555f6fb31c
vishal <vishaldeepverma@live.in>
2023-01-26 19:48:44 +0530

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
