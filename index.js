6b9f47db4f0c74ae99b209ceb5a0fab6b06bd760
vishal <vishaldeepverma@live.in>
2023-01-26 20:01:04 +0530

    added first endpoint

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.send("hello world");
+});
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
